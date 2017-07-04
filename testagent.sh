c29dbd28a5efe6060116fd9d19f5bc29b72ed4c0
Bernardo De Azevedo Moreira <bernardo.az.moreira@gmail.com>
2017-07-04 09:25:46 -0300

    Submodule Test 2 With ls

--- testagent.sh
+++ testagent.sh
@@ -3,5 +3,11 @@
 
 TMPFILE=`mktemp`
 echo $TMPFILE
-echo start. | swipl -s ../agente*.pl > $TMPFILE 2>/dev/null
-less < $TMPFILE
+
+#this is specifically configured to work with a single agent file as a submodule
+#for the wumpus assignment
+for $AGENT in $( ls ../agente*.pl )
+do
+    echo start. | swipl -s ../$AGENT > $TMPFILE 2>/dev/null
+    less < $TMPFILE
+done
