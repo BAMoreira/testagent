3aba7c107d50941998c432971d1bfb341aefd3dc
Bernardo De Azevedo Moreira <bernardo.az.moreira@gmail.com>
2017-07-04 09:33:07 -0300

    Fix Error at swipl Call

--- testagent.sh
+++ testagent.sh
@@ -6,8 +6,9 @@ echo $TMPFILE
 
 #this is specifically configured to work with a single agent file as a submodule
 #for the wumpus assignment
+cd .
 for AGENT in $( ls ../agente*.pl )
 do
-    echo start. | swipl -s ../$AGENT > $TMPFILE 2>/dev/null
+    echo start. | swipl -s $AGENT > $TMPFILE 2>/dev/null
     less < $TMPFILE
 done
