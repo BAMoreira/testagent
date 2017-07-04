5e4a1b789544ffe6d9c15c46de3fce5ca33668ff
Bernardo De Azevedo Moreira <bernardo.az.moreira@gmail.com>
2017-07-04 09:27:49 -0300

    Fix in For Loop

--- testagent.sh
+++ testagent.sh
@@ -6,7 +6,7 @@ echo $TMPFILE
 
 #this is specifically configured to work with a single agent file as a submodule
 #for the wumpus assignment
-for $AGENT in $( ls ../agente*.pl )
+for AGENT in $( ls ../agente*.pl )
 do
     echo start. | swipl -s ../$AGENT > $TMPFILE 2>/dev/null
     less < $TMPFILE
